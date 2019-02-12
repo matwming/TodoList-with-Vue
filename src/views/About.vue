77b3c221ceffa72d719b0685f07d6d2c5853b072
MING WU <matwming114@gmail.com>
2019-02-13 09:29:08 +1100

    test bootstrap

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <nav class="navbar"></nav>
+  <nav class="navbar">
+    <div class="row">
+      <div class="col-md-6" style="border:1px solid black;height:100px">col-md-6</div>
+      <div class="col-md-6" style="border:1px solid green;height:100px">col-md-6</div>
+    </div>
+  </nav>
 </template>
 <style scoped>
 </style>
