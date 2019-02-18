46b398ba57b249d551be2d610c40901f68d9e78a
MING WU <matwming114@gmail.com>
2019-02-18 12:29:49 +1100

    trt to open yeeyi ios

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <h1>Todo List</h1>
+    <h1>Todo List.Added link to yeeyi ios</h1>
     <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
   </header>
